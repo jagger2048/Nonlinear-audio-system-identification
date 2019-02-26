9f87a092a16874558873a220b705951527f0c8a1
jagger <jagger2048@gmail.com>
2019-02-26 17:57:22 +0800

    Add stream generator.

--- SPL/frame.h
+++ SPL/frame.h
@@ -11,11 +11,11 @@ typedef struct {
 
 	size_t sample_rate;			// sample rate
 	size_t frame_size;			// number of samples per chunk package
-}spl_frame_st,*spl_frame_p;
+}spl_frame_st;
 
-spl_frame_p spl_frame_new(size_t sample_rate, size_t frame_size) {
+spl_frame_st * spl_frame_new(size_t sample_rate, size_t frame_size) {
 
-	spl_frame_p pFrame;
+	spl_frame_st * pFrame = (spl_frame_st *)malloc(sizeof(spl_frame_st ));
 	for (size_t channel = 0; channel < 2; channel++)
 	{
 		pFrame->sample_f32[channel] = (float*)malloc(sizeof(float) * frame_size);
@@ -26,17 +26,17 @@ spl_frame_p spl_frame_new(size_t sample_rate, size_t frame_size) {
 	return pFrame;
 }
 
-void static inline spl_frame_load_s16(spl_frame_p pFrame, size_t channel,void* dest, const void* src, size_t count) {
+void static inline spl_frame_load_s16(spl_frame_st * pFrame, size_t channel,void* dest, const void* src, size_t count) {
 	// load |count| s16 fromat pcm samples data from |src| to |dest|
 	memmove( pFrame->sample_s16[channel],src,count * sizeof(int16_t) );	// should be optimized.
 }
 
-void static inline spl_frame_load_f32(spl_frame_p pFrame, size_t channel, void* dest, const void* src, size_t count) {
+void static inline spl_frame_load_f32(spl_frame_st * pFrame, size_t channel, void* dest, const void* src, size_t count) {
 	// load |count| f32 fromat samples data from |src| to |dest| for |channel|
 	memmove(pFrame->sample_f32[channel], src, count * sizeof(float) );	// should be optimized.
 }
 
-int spl_frame_free(spl_frame_p pFrame) {
+int spl_frame_free(spl_frame_st * pFrame) {
 	if (pFrame !=NULL)
 	{
 		for (size_t channel = 0; channel < 2; channel++)
--- SPL/main.cpp
+++ SPL/main.cpp
@@ -3,13 +3,27 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#include "wavfile.h"
 #include "frame.h"
+#include "stream.h"
 int main()
 {
 	printf("This is the signal processing library test case.\n");
-	wav wavfile;
-	spl_frame_p pFram = spl_frame_new(48000, 1024);
+	spl_frame_st *pFrame = spl_frame_new(48000, 1024);
+
+	spl_stream_st *pStream = NULL;
+	pStream = audio_stream_open("music_48k_32bit.wav");
+
+	pcm_stream_frame_get(pStream,pFrame, 1024);
+	for (size_t n = 0; n < 1024; n++)
+	{
+		printf("%d -- %f || %d -- %f\n",
+			pFrame->sample_s16[0][n], 
+			pFrame->sample_f32[0][n], 
+			pFrame->sample_s16[1][n],
+			pFrame->sample_f32[0][n]);
+	}
+	printf("test 2");
+	audio_stream_close(pStream);
 	return 0;
 }
 
